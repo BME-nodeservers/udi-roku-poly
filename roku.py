61e3ea7aaecec47dd2e2c5039937aa77bcc2f4a6
Bob Paauwe <bpaauwe@yahoo.com>
2022-12-09 11:08:35 -0800

    Version 2.0.4

--- roku.py
+++ roku.py
@@ -12,7 +12,7 @@ LOGGER = udi_interface.LOGGER
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([])
-        polyglot.start('2.0.3')
+        polyglot.start('2.0.4')
         roku.Controller(polyglot)
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
